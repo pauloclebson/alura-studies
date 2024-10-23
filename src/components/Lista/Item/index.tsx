ab4a6593dfd61327fc9db82092e954ee49bc7be8
pauloclebson <pauloclebson270993@gmail.com>
2024-10-23 12:52:00 -0300

    criando função de horas para segundos

--- src/components/Lista/Item/index.tsx
+++ src/components/Lista/Item/index.tsx
@@ -1,5 +1,5 @@
 import { ITarefa } from '../../../types/tarefas';
-import style from '../Lista.module.scss';
+import style from './item.module.scss';
 
 interface Props extends ITarefa{
   selecionaTarefa: (tarefaSelecionada: ITarefa) => void
@@ -14,7 +14,6 @@ export default function Item(
     id,
     selecionaTarefa
   }: Props) {
-  console.log("item atual: ",{tarefa, tempo, selecionado, completado, id})
   return(
     <li onClick={() => selecionaTarefa({
       tarefa,
